1dd272d45b20be8f72ab15e7749a3b57614a8a16
Shubham Gehlot <shubhamgehlot675@gmail.com>
2023-09-23 21:05:29 +0530

    updated signUp controller code

--- backend/controllers/auth/signUp.js
+++ backend/controllers/auth/signUp.js
@@ -3,61 +3,73 @@ import { generateToken } from "../../utils/generateToken.js";
 import { hashPassword } from "../../utils/encryption.js";
 
 export const signUpUser = async (req, res) => {
-  const { firstName, lastName, email, password ,pic} = req.body;
+  try {
+    const { firstName, lastName, email, password, pic } = req.body;
 
-  if (!firstName) {
-    res.status(400).json({ success: false, message: "firstName required" });
-  }
-  if (!lastName) {
-    res.status(400).json({ success: false, message: "lastName required" });
-  }
-  if (!email) {
-    res.status(400).json({ success: false, message: "email required" });
-  }
-  if (!password) {
-    res.status(400).json({ success: false, message: "password required" });
-  } else {
-    try {
-      const userExist = await prisma.user.findUnique({
-        where: { email: email },
-        select:{email:true}
-      });
-      if(userExist){
-        res.status(409).json({ success: false, message: "user exist with this email" });
-      }else{
-      const hashedPassword = await hashPassword(password);
-      const user = await prisma.User.create({
-        data: {
-          firstName: firstName,
-          lastName: lastName,
-          email: email,
-          password: hashedPassword,
-          pic:pic,
-        },
-      });
-
-      if (user) {
-        const token = generateToken(user.userId);
-        res.status(201).cookie('access_token', token, {
-        expires: new Date(Date.now() + 8 * 3600000) // cookie will be removed after 8 hours
-      }).json({
-        success:true,
-        token:token,
-        user:{
-        userId: user.userId,
-        firstName: user.firstName,
-        lastName: user.lastName,
-        email:user.email,
-        pic:user.pic,
-      }
-    });
-      }}
-    } catch (error) {
-      console.log("shubham", error);
-      res.status(400).json({
-        success: false,
-        msssage: error.message,
-      });
+    if (!firstName) {
+      return res
+        .status(400)
+        .json({ success: false, message: "firstName required" });
     }
+    if (!lastName) {
+      return res
+        .status(400)
+        .json({ success: false, message: "lastName required" });
+    }
+    if (!email) {
+      return res
+        .status(400)
+        .json({ success: false, message: "email required" });
+    }
+    if (!password) {
+      return res
+        .status(400)
+        .json({ success: false, message: "password required" });
+    }
+    const userExist = await prisma.user.findUnique({
+      where: { email: email },
+      select: { email: true },
+    });
+    if (userExist) {
+      return res
+        .status(409)
+        .json({ success: false, message: "user exist with this email" });
+    }
+    const hashedPassword = await hashPassword(password);
+    const user = await prisma.User.create({
+      data: {
+        firstName: firstName,
+        lastName: lastName,
+        email: email,
+        password: hashedPassword,
+        pic: pic,
+      },
+    });
+
+    if (user) {
+      const token = generateToken(user.userId);
+      res
+        .status(201)
+        .cookie("access_token", token, {
+          expires: new Date(Date.now() + 8 * 3600000), // cookie will be removed after 8 hours
+        })
+        .json({
+          success: true,
+          token: token,
+          user: {
+            userId: user.userId,
+            firstName: user.firstName,
+            lastName: user.lastName,
+            email: user.email,
+            pic: user.pic,
+          },
+        });
+    }
+  } catch (error) {
+    console.log("shubham", error);
+    res.status(400).json({
+      success: false,
+      msssage: error.message,
+    });
   }
 };
